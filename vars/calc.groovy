7afffc61cf83e18ec0ab1ab001ffb1cf6409af1e
SadashivaReddy Pendyala <128224536+sadashiva1234@users.noreply.github.com>
2024-03-11 20:59:49 +0530

    Update calc.groovy

--- vars/calc.groovy
+++ vars/calc.groovy
@@ -1,6 +1,6 @@
 def add(x,y){
-  echo " sum of ${x} and $[y} is ${x+y}"
+  echo "sum of ${x} and ${y} is ${x+y}"
 }
 def sub(x,y){
-  echo " substraction of ${x} and ${y} is ${x-y}"
+  echo "substraction of ${x} and ${y} is ${x-y}"
 }
